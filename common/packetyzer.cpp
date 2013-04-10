fb39a0f57018b5f41c7504a8142ccfff48e57b39
inkooboo <inkooboo@gmail.com>
2013-04-10 16:24:22 +0400

    optimization

--- common/packetyzer.cpp
+++ common/packetyzer.cpp
@@ -4,9 +4,6 @@
 void packetyzer_t::parse_buffer(on_packet_cb_t cb, const char *begin, size_t length)
 {
     const char *end = begin + length;
-    // optimistic scenario
-    // some packets per one buffer and no remanings
-    // no reallocations will occur
     if (m_remaning.empty())
     {
         const char *packet_begin = begin;
@@ -21,7 +18,6 @@ void packetyzer_t::parse_buffer(on_packet_cb_t cb, const char *begin, size_t len
 
         if (packet_begin != end)
         {
-            // save some remanings
             m_remaning.assign(packet_begin, end);
         }
     }
@@ -30,9 +26,7 @@ void packetyzer_t::parse_buffer(on_packet_cb_t cb, const char *begin, size_t len
         m_remaning.reserve(m_remaning.size() + length);
         std::copy(begin, end, &m_remaning[m_remaning.size()]);
 
-        std::vector<char> buff;
-        buff.swap(m_remaning);
-        parse_buffer(cb, &buff[0], buff.size());
+        parse_buffer(cb, &m_remaning[0], m_remaning.size());
     }
 }
 
